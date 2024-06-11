3bbdea1501b434c4173336302bb2c6f970b6a807
AkhatorEnosa <osa@Akhators-MacBook-Pro.local>
2024-06-11 05:02:31 +0100

    compatible location icon

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,10 +1,19 @@
 /* eslint-disable react/prop-types */
-import { MapContainer, TileLayer, Marker, Popup, useMap } from "react-leaflet";
+import { MapContainer, TileLayer, Marker, Popup} from "react-leaflet";
+import { useMap } from "react-leaflet/hooks";
+import "leaflet/dist/leaflet.css";
 
 export const ChangeView = (props) => {
   const map = useMap();
   map.flyTo(props.position, map.getZoom());
   return null;
+};
+
+export const createIcon = (url) => {
+  return new L.Icon({
+    iconUrl: url,
+    iconAnchor: [10, 20],
+  });
 };
 
 const Map = (props) => {
@@ -20,7 +29,7 @@ const Map = (props) => {
           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <Marker position={props.position}>
+        <Marker position={props.position} icon={createIcon("/icon-location.svg")}>
           <Popup className="font-bold text-purple-500">
             {props.details}
           </Popup>
