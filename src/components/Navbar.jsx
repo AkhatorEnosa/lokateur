e99907c168f5c49019a5ba2966f81cadf32f8f7e
AkhatorEnosa <osa@Akhators-MacBook-Pro.local>
2024-06-11 13:39:35 +0100

    finishing touches

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = (props) => {
   return (
     <div className="w-full flex justify-between mb-6 text-white">
       <h1 className="flex justify-center items-center gap-3 text-2xl font-bold tracking-wider"><FaLocationDot /> Lokateur</h1>
-      <p className="underline cursor-pointer" onClick={props.myLocation}>My Location</p>
+      <p className="underline cursor-pointer hover:no-underline" onClick={props.myLocation}>My Location</p>
     </div>
   )
 }
