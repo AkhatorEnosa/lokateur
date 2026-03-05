f53693af7ba58f4ebbf5e129d20159dfbf3c2142
AkhatorEnosa <aosakhogba@gmail.com>
2026-03-05 10:43:33 +0100

    bypassing CORS errors

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  
+  server: {
+    proxy: {
+      '/api-ip': {
+        target: 'https://freeipapi.com',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api-ip/, '/api/json/'),
+      },
+    },
+  },
 })
